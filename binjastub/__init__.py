a0ac9d434cccc177fe261414654a178010544e62
lordidiot <calvin@arthurfong.com>
2022-03-14 11:35:38 +0800

    Fix path hack and update dependencies

--- binjastub/__init__.py
+++ binjastub/__init__.py
@@ -1,9 +1,9 @@
 import os
-import sysconfig as sc
+import z3
 from pathlib import Path
 
 # Hack to include z3 path for virtualenv installs
-bin_path = Path(sc.get_paths()["purelib"]) / "bin"
+bin_path = Path(z3.__file__).parent.parent / "bin"
 if bin_path.exists():
     os.environ["PATH"] = str(bin_path) + ":" + os.environ.get("PATH", "")
 
