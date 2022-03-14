75442fbb21c7b26ca42bf499814abb4bb01515e8
lordidiot <calvin@arthurfong.com>
2022-03-14 11:01:19 +0800

    path hack for z3

--- binjastub/__init__.py
+++ binjastub/__init__.py
@@ -1,1 +1,10 @@
+import os
+import sysconfig as sc
+from pathlib import Path
+
+# Hack to include z3 path for virtualenv installs
+bin_path = Path(sc.get_paths()["purelib"]) / "bin"
+if bin_path.exists():
+    os.environ["PATH"] = str(bin_path) + ":" + os.environ.get("PATH", "")
+
 import mui
