1d83c0be952e8a9d99493450fa573810580c82fa
lordidiot <0xlordidiot@gmail.com>
2022-07-26 21:40:13 +0800

    copy common files

--- plugins/binaryninja/binjastub/__init__.py
+++ plugins/binaryninja/binjastub/__init__.py
@@ -2,6 +2,7 @@ import os
 import z3
 import sys
 from pathlib import Path
+from distutils.dir_util import copy_tree
 
 # Hack to include z3 path for virtualenv installs
 bin_path = Path(z3.__file__).parent.parent / "bin"
@@ -10,5 +11,8 @@ if bin_path.exists():
 
 # Add parent directory to sys.path
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+# Copy common files
+copy_tree("../../common", "../mui/common_resources", update=1)
 
 import mui
