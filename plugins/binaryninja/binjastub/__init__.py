fc8f49d946d63e1ea0e9c11107dd4e0e8cc481d3
lordidiot <0xlordidiot@gmail.com>
2022-07-26 15:02:05 +0800

    add import path

--- plugins/binaryninja/binjastub/__init__.py
+++ plugins/binaryninja/binjastub/__init__.py
@@ -1,10 +1,14 @@
 import os
 import z3
+import sys
 from pathlib import Path
 
 # Hack to include z3 path for virtualenv installs
 bin_path = Path(z3.__file__).parent.parent / "bin"
 if bin_path.exists():
     os.environ["PATH"] = str(bin_path) + ":" + os.environ.get("PATH", "")
+
+# Add parent directory to sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 import mui
